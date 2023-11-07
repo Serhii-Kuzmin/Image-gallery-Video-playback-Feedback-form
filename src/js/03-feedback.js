bd6d520d5565417c2a3149f6c5eab8cf17b41fd4
Serhii-Kuzmin <seryy8248@gmail.com>
2023-11-07 23:35:19 +0100

    commit-update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,44 +1,3 @@
-// import throttle from 'lodash/throttle';
-
-// const feedbackForm = document.querySelector('.feedback-form');
-// const emailInput = feedbackForm.querySelector('input[name="email"]');
-// const messageInput = feedbackForm.querySelector('textarea[name="message"]');
-// const localStorageKey = 'feedback-form-state';
-
-// emailInput.addEventListener('input', throttle(onInput, 500));
-// messageInput.addEventListener('input', throttle(onInput, 500));
-
-// function onInput() {
-//   const formData = {
-//     email: emailInput.value,
-//     message: messageInput.value,
-//   };
-//   localStorage.setItem(localStorageKey, JSON.stringify(formData));
-// }
-
-// const savedData = localStorage.getItem(localStorageKey);
-// if (savedData) {
-//   const formData = JSON.parse(savedData);
-//   emailInput.value = formData.email;
-//   messageInput.value = formData.message;
-// }
-
-// feedbackForm.addEventListener('submit', onSubmit);
-
-// function onSubmit(event) {
-//   event.preventDefault();
-
-//   const formData = {
-//     email: emailInput.value,
-//     message: messageInput.value,
-//   };
-//   console.log(formData);
-
-//   localStorage.removeItem(localStorageKey);
-// }
-
-//_______________________________________
-
 import throttle from 'lodash/throttle';
 
 const form = document.querySelector('.feedback-form');
@@ -46,7 +5,7 @@ const emailInput = form.querySelector('input[name="email"]');
 const messageTextarea = form.querySelector('textarea[name="message"]');
 const storageKey = 'feedback-form-state';
 
-// Отримайте дані з локального сховища та заповніть поля форми, якщо вони є
+
 const storedData = localStorage.getItem(storageKey);
 
 if (storedData) {
@@ -59,7 +18,6 @@ if (storedData) {
   }
 }
 
-// Відстежування події input і збереження даних в локальному сховищі
 form.addEventListener('input', throttle(() => {
   const dataToStore = {
     email: emailInput.value,
@@ -68,7 +26,7 @@ form.addEventListener('input', throttle(() => {
   localStorage.setItem(storageKey, JSON.stringify(dataToStore));
 }, 500));
 
-// Під час сабміту форми очистити локальне сховище
+
 form.addEventListener('submit', () => {
   localStorage.removeItem(storageKey);
 
